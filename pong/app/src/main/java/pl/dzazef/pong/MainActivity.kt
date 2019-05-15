1a212b2b315faac1d7f0959e8a4e173392d340a9
dzazef <dzazef@gmail.com>
2019-05-16 00:27:51 +0200

    Pong: hide on screen buttons

--- pong/app/src/main/java/pl/dzazef/pong/MainActivity.kt
+++ pong/app/src/main/java/pl/dzazef/pong/MainActivity.kt
@@ -2,12 +2,22 @@ package pl.dzazef.pong
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.decorView.systemUiVisibility = (SYSTEM_UI_FLAG_IMMERSIVE
+                or SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or SYSTEM_UI_FLAG_FULLSCREEN
+                or SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
     }
 
 
